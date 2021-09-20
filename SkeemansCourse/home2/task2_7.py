366a19adb81f7900dd84c8e10413fa148a469e0a
wodograyvk <wodograyvk@gmail.com>
2021-09-21 00:34:32 +0300

    1

---
+++ SkeemansCourse/home2/task2_7.py
@@ -0,0 +1,21 @@
+def is_prime(number):
+    if number < 2:
+        return False
+
+    for i in range(2, number // 2 + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+def first_prime_numbers(n):
+    result = []
+    current_number = 1
+    while len(result) < n:
+        if is_prime(current_number):
+            result.append(current_number)
+        current_number += 1
+
+    return  result
+
+print(first_prime_numbers(12))
